ed774c20dc1a1f1ece1d5cdc58914edb9e70e0da
heru <h312u_kurniawan@yahoo.com>
2017-02-01 23:59:37 +0700

    update header logo

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -31,7 +31,7 @@
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                         <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
                     </button>
-                    <a class="navbar-brand page-scroll" href="#page-top"><i class="fa fa-car">IMoov Car Rental</i></a>
+                    <a class="navbar-brand page-scroll" href="#page-top"><i class="fa fa-car"></i> IMoov Car Rental</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
